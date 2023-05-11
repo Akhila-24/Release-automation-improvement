14391231d5b8d15ba66158fafc85863c46e6963a
Akhila Ubidi <akhila.ubidi@thoughtworks.com>
2023-05-11 20:00:39 +0530

    added jenkinsfile

--- curl.sh
+++ curl.sh
@@ -1,2 +1,3 @@
 curl -X GET http://localhost:8080/job/job/config.xml -u $1:$2 -o tempconfig.xml
-curl -X POST http://localhost:8080/job/createItem?name=job1 -u $1:$2 -H 'contentType:application/xml' --data tempconfig.xml
+cat tempconfig.xml
+curl -X POST http://localhost:8080/job/createItem?name=job1 -u $1:$2 --data-binary @tempconfig.xml -H "Content-Type:text/xml"
